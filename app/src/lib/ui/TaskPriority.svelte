f20f6565c467a4d8ddaf74c0ff29e05e1b44e808
Dmitriy Din <dmitriy1d01@gmail.com>
2023-11-11 19:13:52 +0300

    Added task page

---
+++ app/src/lib/ui/TaskPriority.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  export let value = ''
+  export let disabled = false;
+
+  const priority = {
+    '0': { title: 'Низкий', colors: 'bg-info text-info-content' },
+    '1': { title: 'Средний', colors: 'bg-warning text-warning-content' },
+    '2': { title: 'Высокий', colors: 'bg-error text-error-content' }
+  };
+</script>
+
+<select
+  {disabled}
+  class="{$$props.class} ml-2 font-bold rounded-xl px-3 py-1 pr-2 {priority[value].colors}"
+  bind:value
+  on:change
+>
+  {#each Object.entries(priority) as [index, p]}
+    <option value={index}>{p.title}</option>
+  {/each}
+</select>
---
+++ app/src/lib/ui/TaskStatus.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  export let value = '';
+  export let disabled = false;
+
+  const status = {
+    '0': { title: 'Назначена', colors: 'bg-info text-info-content' },
+    '1': { title: 'В работе', colors: 'bg-warning text-warning-content' },
+    '2': { title: 'Завершена', colors: 'bg-success text-success-content' }
+  };
+</script>
+
+<select
+  {disabled}
+  class="{$$props.class} ml-2 font-bold rounded-xl px-3 py-1 pr-2 {status[value].colors}"
+  bind:value
+  on:change
+>
+  {#each Object.entries(status) as [index, s]}
+    <option value={index}>{s.title}</option>
+  {/each}
+</select>
